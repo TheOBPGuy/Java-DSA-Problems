dd62ca97db0eb4bdf4eab5cbb57de2398a7d612c
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-11-26 18:12:00 -0500

    Balanced String Split - Java

---
+++ BalancedStringSplit.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+/**
+ * 
+ */
+
+/**
+ * @author farhanibrahim
+ *
+ */
+public class BalancedStringSplit {
+
+    public int balancedStringSplit(String s) {
+        int res = 0;
+        // Find out all the substrings of a string
+        LinkedList<String> subStr = new LinkedList<String>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                subStr.add(s.substring(i, j));
+            }
+        }
+
+        for (int k = 0; k < subStr.size(); k++) {
+
+        }
+
+        return res;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
